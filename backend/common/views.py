d301db519b6be60795d199769a203058a2652482
MohsenSami <mohsensami88@gmail.com>
2022-10-10 21:38:16 +0330

    register api

---
+++ backend/common/views.py
@@ -0,0 +1,19 @@
+from rest_framework import exceptions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer
+
+
+class RegisterAPIView(APIView):
+    def post(self, request):
+        data = request.data
+
+        if data['password'] != data['password_confirm']:
+            raise exceptions.APIException('Passwords do not match!')
+
+        data['is_ambassador'] = 0
+
+        serializer = UserSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
