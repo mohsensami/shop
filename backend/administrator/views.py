ee69a043ef3526bb477202ca5f3b6d5bb48a9fcd
MohsenSami <mohsensami88@gmail.com>
2022-10-11 23:15:39 +0330

    orders

--- backend/administrator/views.py
+++ backend/administrator/views.py
@@ -3,10 +3,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, LinkSerializer, OrderSerializer
 from common.authentication import JWTAuthentication
 from common.serializers import UserSerializer
-from core.models import User, Product
+from core.models import User, Product, Link, Order
 
 
 class AmbassadorAPIView(APIView):
@@ -45,3 +45,23 @@ class ProductGenericAPIView(
     def delete(self, request, pk=None):
         response = self.destroy(request, pk)
         return response
+
+
+class LinkAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk=None):
+        links = Link.objects.filter(user_id=pk)
+        serializer = LinkSerializer(links, many=True)
+        return Response(serializer.data)
+
+
+class OrderAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        orders = Order.objects.filter(complete=True)
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data)
