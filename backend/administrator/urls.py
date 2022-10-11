f0903967aba910a15d845d7366d328aa6e52be32
MohsenSami <mohsensami88@gmail.com>
2022-10-11 22:40:04 +0330

    products Generic class and Faker

--- backend/administrator/urls.py
+++ backend/administrator/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from .views import AmbassadorAPIView
+from .views import AmbassadorAPIView, ProductGenericAPIView
 
 urlpatterns = [
     path('', include('common.urls')),
     path('ambassadors/', AmbassadorAPIView.as_view()),
+    path('products/', ProductGenericAPIView.as_view()),
+    path('products/<str:pk>/', ProductGenericAPIView.as_view()),
 ]
--- backend/core/models.py
+++ backend/core/models.py
@@ -50,3 +50,10 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
 
     objects = UserManager()
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=1000, null=True)
+    image = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
