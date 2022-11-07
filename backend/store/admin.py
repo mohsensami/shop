5b820a91a8833268f186cda63b64366908d4fcb6
MohsenSami <mohsensami88@gmail.com>
2022-11-07 22:39:57 +0330

    Backend => fix bug in collection admin panel

--- backend/store/admin.py
+++ backend/store/admin.py
@@ -70,7 +70,7 @@ class CollectionAdmin(admin.ModelAdmin):
     @admin.display(ordering='products_count')
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
-            products_count=Count('product')
+            products_count=Count('products')
         )
 
 
